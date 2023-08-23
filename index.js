bd60b23f5ae00ea7d80fa78f7bfd33bd1475a52b
mursalin@gmail.com <MURSA>
2023-08-24 03:09:00 +0600

    fixed the vercel deployment issue and now server working

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const productsData = require("./data.json"); // Import your data.json
 
-// Serve the data.json file as a JSON response
-app.get("/data", (req, res) => {
-  res.sendFile(__dirname + "/data.json");
+app.get("/", (req, res) => {
+  res.send("JSON server running");
 });
 
-// Start the server
+app.get("/products", (req, res) => {
+  res.json(productsData);
+});
+
+// Get product by ID
+app.get("/products/:id", (req, res) => {
+  const productId = parseInt(req.params.id);
+  const product = productsData.find((item) => item.id === productId);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ message: "Product not found" });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
