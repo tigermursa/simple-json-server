1901cf6ed711eafd3b60711c0eb152ee6643c5c9
mursalin@gmail.com <MURSA>
2023-08-24 22:41:51 +0600

    added jwt token api

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const cors = require("cors"); // Import the cors package
 const app = express();
+require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 const productsData = require("./data.json"); // Import your data.json
+const jwt = require("jsonwebtoken");
+// JWT verify middleware code here ..................................
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "Unauthorized access" });
+  }
+  const token = authorization.split(" ")[1];
 
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "Unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 // Use the cors middleware
 app.use(cors());
 
@@ -19,7 +40,14 @@ app.get("/products", (req, res) => {
 app.get("/products/:id", (req, res) => {
   const productId = parseInt(req.params.id);
   const product = productsData.find((item) => item.id === productId);
-
+  // jwt api
+  app.post("/jwt", (req, res) => {
+    const user = req.body;
+    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: "3h",
+    });
+    res.send({ token });
+  });
   if (product) {
     res.json(product);
   } else {
